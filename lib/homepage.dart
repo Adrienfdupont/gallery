ffdace090ea6820bc67ec31cb9a81770398889b1
Ryujin42 <ryujin.hector@gmail.com>
2024-02-07 14:34:47 +0100

    Initial Commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:gallery/data_objects/picture_data.dart';
 
+import 'package:http/http.dart' as http;
+
 import 'dart:async';
 import 'dart:convert';
 
@@ -30,8 +32,15 @@ class Homepage extends StatelessWidget {
   }
 
   Future<List<PictureData>> _fetchPictureData() async {
-    final pictures = await rootBundle.loadString('assets/pictures.json');
-    final parsed = (jsonDecode(pictures) as List).cast<Map<String, dynamic>>();
-    return parsed.map<PictureData>((json) => PictureData.fromJson(json)).toList();
+    var response = await http.get(Uri.parse("https://api.unsplash.com/photos/?client_id=Va7WM5ToNpyz8-4CWvZ7fkeV8sr_QMf0BH9NAJ8GCbk&page=1&per_page=30"));
+    if (response.statusCode == 200) {
+      final pictures = response.body;
+      final parsed = (jsonDecode(pictures) as List).cast<Map<String, dynamic>>();
+      return parsed.map<PictureData>((json) => PictureData.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load pictures');
+    }
+
   }
 }
+
